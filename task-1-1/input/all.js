6d378ec041df5310e4736c8a0765ed6b50b335ba
Misha Kushka <kushka.misha@gmail.com>
2020-12-17 17:54:15 +0200

    Task 1.1

---
+++ task-1-1/input/all.js
@@ -0,0 +1,12 @@
+// This is a simple program
+const abs = a => Math.abs(a)
+
+/* This is some comment */
+// Another one
+console.log(abs(-10))
+/*
+Try again
+*/
+console.log(abs(5))
+console.log("/*Do not ignore this //:)*/")
+
---
+++ task-1-1/input/simple.js
@@ -0,0 +1,9 @@
+// Comment
+console.log(1)
+// another one //
+console.log(2)
+//
+// Line
+//
+console.log(3)
+//
